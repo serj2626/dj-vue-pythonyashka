231c666f45a2354c803a37d89ac7afc2902e9745
serj2626 <serj2626@mail.ru>
2024-06-15 14:24:00 +0300

    added models in admin panel

--- backend/studies/admin.py
+++ backend/studies/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Subject, Level, Lesson
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    '''Admin View for Subject)'''
+
+    list_display = ('title', 'level', 'slug', )
+    # list_filter = ('',)
+    # inlines = [
+    #     Inline,
+    # ]
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    # ordering = ('',)
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    '''Admin View for Level)'''
+
+    list_display = ('title', 'slug', )
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    '''Admin View for Lesson)'''
+
+    list_display = ('title', 'slug',  'subject', )
--- backend/studies/apps.py
+++ backend/studies/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StudiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'studies'
+    verbose_name = 'Учеба'
